3acc97a68c0f21ce6332d4a56222631724016f04
sofiamorgillo <sofia.morgillo@student.fairfield.edu>
2024-10-16 08:39:03 -0400

    Update Main.java

--- Programming Workshop Lab Semester Project/src/books/Main.java
+++ Programming Workshop Lab Semester Project/src/books/Main.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException{
-        String libraryPath = "C:\\Users\\Nazli\\IdeaProjects\\Programming Workshop Lab Semester Project\\Library";
+        String libraryPath = "C:\\Users\\Nazli\\IdeaProjects\\Programming Workshop Lab Semester Project\\Library";//stores the path to the library containing the topics and articles for each topic
 
-        Library library = new Library(libraryPath);
-        ArrayList<Topic> topics = library.getTopics();
+        Library library = new Library(libraryPath);//creates a new library given the path
+        ArrayList<Topic> topics = library.getTopics();//creates an arraylist of the topics
 
-        for(Topic topic: topics){
+        for(Topic topic: topics){//for each topic in the arraylist an the article is processed using the ...
             topic.processArticles();
         }
     }
