afa10fc0b55330039604cd42660ac092bfe96a8a
sofiamorgillo <sofia.morgillo@student.fairfield.edu>
2024-10-16 08:40:13 -0400

    Update Main.java

--- Programming Workshop Lab Semester Project/src/books/Main.java
+++ Programming Workshop Lab Semester Project/src/books/Main.java
@@ -10,7 +10,7 @@ public class Main {
         Library library = new Library(libraryPath);//creates a new library given the path
         ArrayList<Topic> topics = library.getTopics();//creates an arraylist of the topics
 
-        for(Topic topic: topics){//for each topic in the arraylist an the article is processed using the ...
+        for(Topic topic: topics){//for each topic in the arraylist an the article is processed using the processArticles funtion in the topic class
             topic.processArticles();
         }
     }
